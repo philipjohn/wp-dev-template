da0a0995d99cb60b37875cb1a36052ee42dce5c8
Philip John <phil@philipjohn.co.uk>
2014-02-07 17:10:41 +0000

    Turn debug on, and direct file transfer

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -78,7 +78,9 @@ define('WPLANG', '');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
+define( 'WP_DEBUG', true );
+
+define( 'FS_METHOD', 'direct' );
 
 /* That's all, stop editing! Happy blogging. */
 
