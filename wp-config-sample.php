26be1369232d571915179237d9ff4b87ed9cac5c
Philip John <phil@philipjohn.co.uk>
2014-02-07 17:11:32 +0000

    3.8.1

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -78,9 +78,7 @@ define('WPLANG', '');
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define( 'WP_DEBUG', true );
-
-define( 'FS_METHOD', 'direct' );
+define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
